0fb6d0193564d4e6239cd3235d9aa35ad00b50ea
Anika Gunaselan <gunaselananika@gmail.com>
2024-06-11 11:11:30 -0500

    Initial Commit

---
+++ ToDoListAnika/ToDoListAnikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAnikaApp.swift
+//  ToDoListAnika
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAnikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
