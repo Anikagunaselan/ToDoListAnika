30065d06e18d7ac96122d1a8250fbc1e630ddfee
Anika Gunaselan <gunaselananika@gmail.com>
2024-06-14 12:02:06 -0500

    To Do List App

---
+++ ToDoItem.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoItem.swift
+//  ToDoListAnika
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import Foundation
---
+++ ToDoListAnika/NewToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoView.swift
+//  ToDoListAnika
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
